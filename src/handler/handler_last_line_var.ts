6341e0d56fb15b95abd2e19b1137a5f3c4eeb2a9
test <test@qq.com>
2020-11-18 22:10:18 +0800

    fix last_line_var

--- src/handler/handler_last_line_var.ts
+++ src/handler/handler_last_line_var.ts
@@ -4,17 +4,25 @@ export function last_line_var(textEditor: vscode.TextEditor, edit: vscode.TextEd
     let cursor = textEditor.selection.active;
     let document = textEditor.document;
     let line = document.lineAt(cursor.line);
-    let last_line = document.lineAt(cursor.line - 1);
-    let ident = line.firstNonWhitespaceCharacterIndex;
-    let last_op_index = -1
-    last_op_index = last_line.text.indexOf(":=")
-    if (last_op_index == -1) {
-        last_op_index = last_line.text.indexOf("=")
+    for (let i = cursor.line - 1; i > 0; i++) {
+        let last_line = document.lineAt(i);
 
+        let ident = line.firstNonWhitespaceCharacterIndex;
+        let last_op_index = -1
+        last_op_index = last_line.text.indexOf(":=")
+        if (last_op_index == -1) {
+            last_op_index = last_line.text.indexOf("=")
+
+        }
+        if (last_op_index == -1) {
+            if (i <= 0) {
+                throw new Error(`cannot find op in last line [${last_line.text}]`)
+            }
+            continue;
+
+        }
+        let last_variable = last_line.text.slice(ident, last_op_index)
+        edit.insert(cursor, last_variable);
     }
-    if (last_op_index == -1) {
-        throw new Error(`cannot find op in last line [${last_line.text}]`)
-    }
-    let last_variable = last_line.text.slice(ident, last_op_index)
-    edit.insert(cursor, last_variable);
+
 }
